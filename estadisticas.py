db95f0c848158b79a927f90bdeb2b6ff6a8bf2f0
lauralardies <lrodrlar@myuax.com>
2022-04-28 10:17:49 +0200

    ataque

--- estadisticas.py
+++ estadisticas.py
@@ -9,15 +9,23 @@ df_resumen = df.describe().transpose()
 print("Valores analizados de los pokemon del archivo .csv")
 print (df_resumen)
 
+# Vista general de los datos
 plt.figure(figsize=(12,8))
 plt.title("Atributos")
 sns.boxplot(data=df)
+plt.show()
+
+# Tabla de Ataque 
+plt.figure(figsize=(12,10))
+sns.displot(x = "Attack", data = df, kde = True)
+plt.suptitle("Distribution of Attack stats")
 plt.show()
 
 df2=df.copy()
 df2 = df2.drop(['Generation', 'Legendary','Total'],1)
 df2 = pd.melt(df2, id_vars=["Name", "Type 1", "Type 2"], var_name="Stat")
 
+# Estudia en rendimiento general de cada pokemon
 plt.figure(figsize=(20,10))
 plt.title("Rendimiento")
 plt.xlabel("Value of Feature")
