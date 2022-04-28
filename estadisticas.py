496764f5574d29ba67c7e3088a7bd898dd8d5dea
lauralardies <lrodrlar@myuax.com>
2022-04-28 10:08:59 +0200

    arreglos

--- estadisticas.py
+++ estadisticas.py
@@ -6,10 +6,11 @@ sns.set_style('white')
 df = pd.read_csv("Pokemon.csv", ",", index_col=0)
 df_resumen = df.describe().transpose()
 
+print("Valores analizados de los pokemon del archivo .csv")
 print (df_resumen)
 
 plt.figure(figsize=(12,8))
-plt.title("How are the Attributes distributed?")
+plt.title("Atributos")
 sns.boxplot(data=df)
 plt.show()
 
@@ -18,11 +19,7 @@ df2 = df2.drop(['Generation', 'Legendary','Total'],1)
 df2 = pd.melt(df2, id_vars=["Name", "Type 1", "Type 2"], var_name="Stat")
 
 plt.figure(figsize=(20,10))
-plt.title("How are the Performance Attributes distributed?")
+plt.title("Rendimiento")
 plt.xlabel("Value of Feature")
 sns.histplot(data=df2)
-plt.show()
-
-sns.catplot(x='Legendary',kind='count',data=df,height=6,aspect=1)
-plt.title("Legendary Pokemons are less than 12% of all Pokemons")
 plt.show()
